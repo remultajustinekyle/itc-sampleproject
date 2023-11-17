1e4915ebe6050df306aa89190c538e8ec2f3408f
remultajustinekyle <justinekyleremulta2@gmail.com>
2023-11-17 11:15:27 +0800

    added remulta 2

---
+++ src/kyle/oop_basics.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kyle;
+
+/**
+ *
+ * @author CRISTINA P. SUELLO
+ */
+public class oop_basics {
+    public static void main (String[] args){
+        System.out.println("taas");
+    }
+}
+
+
---
+++ src/kyle/wapa.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kyle;
+
+/**
+ *
+ * @author CRISTINA P. SUELLO
+ */
+public class wapa {
+    public static void main (String[] args){
+        System.out.println(" remulta 2");
+    }
+}
